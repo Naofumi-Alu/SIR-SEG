2b1554f9ae774182b4cc95990614c107df6b4e85
Alu-Naofumi <joseabrahammaciastorres@gmail.com>
2022-03-17 03:59:54 -0500

    SIR-SEG V1.0.0 Configurando Sass- react babel y webpack configurado

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports={
+    entry: "./src/index.js",
+    mode: "development",
+    module:{
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "babel-loader",
+                options: {presets: ["@babel/env"]}
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    resolve: {extensions: ["*", ".js", ".jsx"]},
+    output: {
+        path: path.resolve(__dirname, "build/"),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "build/")
+        },
+        port: 5000,
+        hot: true,
+        open:true
+    }
+
+}
