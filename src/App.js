7d7fef0ea10f32a30aadee10ce1db91871a09c89
Alu-Naofumi <joseabrahammaciastorres@gmail.com>
2022-03-17 04:14:02 -0500

    SIR-SEG V1.0.1 Configurando pluguins Html y MiniPluguinCss, configuracion babel , jsx, sass completa

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, {Component} from "react";
 import {hot} from "react-hot-loader";
 import "./App.css";
+import "./App2.scss";
 
 class App extends Component {
     render(){
         return(
+        <>
             <div className="App">
                 <h1>Hello World</h1>
             </div>
+            <div className="container">
+                Usando SASS
+            </div>
+        </>
         );
     }
 }
